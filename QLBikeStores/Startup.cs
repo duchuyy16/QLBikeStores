e31bcf8a69682ce4b17a6b82b356bbae08d6bbf5
Bui DucHuy <duchuyy16@gmail.com>
2022-11-14 23:59:04 +0700

    update 14/11/2022

--- QLBikeStores/Startup.cs
+++ QLBikeStores/Startup.cs
@@ -26,7 +26,7 @@ namespace QLBikeStores
         {
             services.AddControllersWithViews();
             services.AddDbContext<demoContext>();
-            
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
