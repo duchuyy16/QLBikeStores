c83287fd0d55651a41cce9739ed18859cb64352c
Bui DucHuy <duchuyy16@gmail.com>
2023-01-15 09:34:28 +0700

    update 15/1/2023

--- QLBikeStores/Controllers/PaymentController.cs
+++ QLBikeStores/Controllers/PaymentController.cs
@@ -2,6 +2,9 @@
 using QLBikeStores.Models;
 using System.Collections.Generic;
 using System;
+using Microsoft.AspNetCore.Http;
+using QLBikeStores.Helpers;
+
 namespace QLBikeStores.Controllers
 {
     public class PaymentController : Controller
@@ -14,45 +17,43 @@ namespace QLBikeStores.Controllers
 
         public IActionResult Index()
         {
+            
+            if (HttpContext.Session.GetString("Username") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                //lay thong tin gio hang
+                var data = HttpContext.Session.Get<List<CartItem>>("GioHang");
+                //gan du lieu cho order
+                Order ord = new Order();
+                ord.OrderStatus = 4;
+                ord.OrderDate = DateTime.Now;
+                ord.RequiredDate = DateTime.Today.AddDays(3);
+                ord.ShippedDate = DateTime.Today.AddDays(2);
+                ord.StoreId = 1;
+                ord.StaffId = 1;
+                _context.Orders.Add(ord);
+                _context.SaveChanges();
 
-            //if (Session["idUser"] == null)
-            //{
-            //    return RedirectToAction("Login", "Account");
-            //}
-            //else
-            //{
-            //    //lay thong tin gio hang
-            //    var cart = HttpContext.Session.Get<List<CartItem>>("GioHang");
-            //    //gan du lieu cho order
-            //    Order ord = new Order();
-            //    ord.OrderId =;
-            //    ord.CustomerId =
-            //    ord.OrderStatus = 4;
-            //    ord.OrderDate = DateTime.Now;
-            //    ord.RequiredDate = DateTime.Today.AddDays(3);
-            //    ord.ShippedDate = DateTime.Today.AddDays(2);
-            //    ord.StoreId = 1;
-            //    ord.StaffId = 1;
-            //    _context.Orders.Add(ord);
-            //    _context.SaveChanges();
+                //lay orderid vua moi tao de luu vao bang orderitems
+                int orderId = ord.OrderId;
 
-            //    //lay orderid vua moi tao de luu vao bang orderitems
-            //    int orderId = ord.OrderId;
+                List<OrderItem> lstOrderItem = new();
+                foreach (var item in data)
+                {
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.OrderId = orderId;
+                    orderItem.ProductId = item.ProductId;
+                    orderItem.Quantity = item.Quantity;
+                    orderItem.ListPrice = item.ListPrice;
+                    lstOrderItem.Add(orderItem);
+                }
 
-            //    List<OrderItem> lstOrderItem = new();
-            //    foreach (var item in lstCart)
-            //    {
-            //        OrderItem orderItem = new OrderItem();
-            //        orderItem.OrderId = orderId;
-            //        orderItem.ProductId = item.ProductId;
-            //        orderItem.Quantity = item.Quantity;
-            //        orderItem.ListPrice = item.ListPrice;
-            //        lstOrderItem.Add(orderItem);
-            //    }
-
-            //    _context.OrderItems.AddRange(lstOrderItem);
-            //    _context.SaveChanges();
-            //}
+                _context.OrderItems.AddRange(lstOrderItem);
+                _context.SaveChanges();
+            }
             return View();
         }
     }
